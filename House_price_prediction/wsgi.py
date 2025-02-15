f1925359bd36d21a0cae5c56a739cd36501507ae
Mohan5488 <158578521+Mohan5488@users.noreply.github.com>
2025-02-15 12:08:22 +0530

    Update wsgi.py

--- House_price_prediction/wsgi.py
+++ House_price_prediction/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "House_price_prediction.settings")
 
 application = get_wsgi_application()
+
+app = application
