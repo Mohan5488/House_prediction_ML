d9067e512be8c265772812b1bf0b8be27a4f9577
Mohan5488 <158578521+Mohan5488@users.noreply.github.com>
2025-02-15 13:12:34 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-if command -v python3.9 &>/dev/null; then
-    python3.9 manage.py collectstatic --noinput
-elif command -v python3 &>/dev/null; then
-    python3 manage.py collectstatic --noinput
-else
-    python manage.py collectstatic --noinput
-fi
+python3 manage.py collectstatic --noinput
+
